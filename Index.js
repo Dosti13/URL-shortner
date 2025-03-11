6a911cb24ef5e264a0f4880fbc6717bfe9e53a0b
Dosti13 <dostikhan41@gmail.com>
2025-03-11 07:50:59 -0700

    add something

--- Index.js
+++ Index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+require("dotenv").config()
 const app = express()
 const port = process.env.PORT || 4000
 const urlroute  = require('./route/url')
@@ -8,7 +9,7 @@ const path = require("path")
 const connect = require ('./db/db')
 const cookieparser = require("cookie-parser")
 const {checkForAuthentication,restrict} = require("./middlewear/Auth")
-require("dotenv").config()
+
 
 app.set("view engine","ejs")
 app.set("views", path.join(__dirname, "views"));
